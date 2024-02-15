0b3580ca98f15c1cedeae5a12602d00a7f0f9570
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-02-15 22:57:13 +0530

    Add files via upload

---
+++ Differnce between numpy array and list.py
@@ -0,0 +1,9 @@
+#A list cannot directly handle mathematical operations,while array can 
+#An array consumes less memory than a list  
+# using am array is faster than a list 
+# a list can store diffrent  datatypes  while you can't do that in an Array
+# A list is easier  to modify  since a list store  each ellements  indviduvaly ,it  is easier to add  
+#and delete an elements than an array does 
+# In list one elemets can have nested data with diffrence size ,while you cannot do the  same in array 
+# array homogenous 
+#list are hetrogeneous 
