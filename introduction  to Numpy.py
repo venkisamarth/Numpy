f610fc052c1c921eac6676ce51c01e155a6d17f4
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-02-15 21:28:38 +0530

    Add files via upload

---
+++ introduction  to Numpy.py
@@ -0,0 +1,18 @@
+# Numpy is the short from of Numerical Python 
+# In 2005 ,Travis oliphant created Numpy  pakage
+# Numpy is a pakage that defines a multi-dimentional array object and associative fast math function that 
+# operate that operate on 
+# It also has funciton for working in domin of linear algibara Fourier Transformation and Matrices 
+#In sample words. it is the fundamental pakage for scintific  in python 
+
+#2 what are array 
+#1 An array is defined as a collection of items that are stored at contiguous memory locations 
+# It is a contains which cah hold a fixed  number of items, and thes should these should be of the same type 
+# A combination of array saves a lot of times The array can reduce  the over  rall size of the code 
+
+
+# 3 Advanatages  of using numpy array 
+# 1> Numpy  uses much less memory to store data 
+# 2> Numpy makes it externaly easy to perform  mathematical  operations  on it 
+# 3> used for the creation of n_dimational  array 
+# 4> Finding  elements in Numpy array is eassy 
